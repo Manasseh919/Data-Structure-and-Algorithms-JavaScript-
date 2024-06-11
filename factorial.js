139c982f58ccd055e987bca5925a5640e6186dab
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2024-06-11 11:29:13 +0200

    Fibonacci and factorial

---
+++ factorial.js
@@ -0,0 +1,13 @@
+function Factorial(n){
+    let result = 1
+    if(n == 0){
+        return false
+    }
+    for (let i = 2;i<=n ;i++){
+        result = result * i
+    }
+    return result
+}
+
+
+console.log(Factorial(5))

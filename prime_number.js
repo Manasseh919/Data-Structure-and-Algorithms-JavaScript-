8b54542d7a395fbf2f84151031050a46b1264f03
Manasseh Ameyow <manasseh919@gmail.com>
2024-06-12 08:33:42 +0000

    optimsed primenumbers

--- prime_number.js
+++ prime_number.js
@@ -1,15 +1,15 @@
-function isPrime(n){
-    if(n<2){
-        return false
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i < Math.sqrt(n); i++) {
+    if (n % i == 0) {
+      return false;
     }
-    for(let i = 2;i<n;i++){
-        if(n%i==0){
-            return false
-        }
-    }
-    return true
+  }
+  return true;
 }
 
-console.log(isPrime(5))
+console.log(isPrime(5));
 
-//Big-O = O(n)
+//Big-O = O(sqrt(n))
